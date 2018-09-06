625c078141854e393616e2ac2ad761e052ffad9a
Jared Forsyth <jared@jaredforsyth.com>
2018-09-05 21:48:32 -0600

    add a dockerfile server example

--- server/hello-reason/bin/Server.ml
+++ server/hello-reason/bin/Server.ml
@@ -1,17 +1,17 @@
-(* client_example.ml *)
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let body =
-  Client.get (Uri.of_string "https://www.reddit.com/") >>= fun (resp, body) ->
-  let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
-  body
+let server =
+  let callback _conn req body =
+    let uri = req |> Request.uri |> Uri.to_string in
+    let meth = req |> Request.meth |> Code.string_of_method in
+    let headers = req |> Request.headers |> Header.to_string in
+    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
+      (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
+         uri meth headers body))
+    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+  in
+  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
 
-let () =
-  let body = Lwt_main.run body in
-  print_endline ("Received body\n" ^ body)
+let () = ignore (Lwt_main.run server)
