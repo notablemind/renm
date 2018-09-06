03f97d2f809fc55051f29c523929448a6c2f7a4f
Jared Forsyth <jared@jaredforsyth.com>
2018-09-05 23:30:09 -0600

    figuring out now secrets

---
+++ server/secret/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Hello, world!' + JSON.stringify({
+            'env': process.env.DOT_EN_V,
+            'now.json': process.env.NOWJSON,
+            'cli': process.env.CLI
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+
+        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+    }
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
