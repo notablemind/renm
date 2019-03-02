efcb14333902c64ab9c709570ba43acc82ab299c
Jared Forsyth <jared@jaredforsyth.com>
2019-03-02 07:43:29 -0700

    convert markdown parsed stuff

--- convert-legacy.js
+++ convert-legacy.js
@@ -1,18 +1,88 @@
 
 const input = require('./structure-editor-literature-review.nm.json')
+const remark = require('../nm/node_modules/remark')
 
 const map = {}
 const treeToMap = (parent, depth) => node => {
   map[node.id] = node;
   node.parent = parent
-  if (depth > 0) {
-    node.children = []
-  }
+  // if (depth > 0) {
+  //   node.children = []
+  // }
   node.children.forEach(treeToMap(node.id, depth + 1))
 }
 treeToMap(input.root.id, 0)(input.root)
 
 // TODO parse markdown yall
+const processor = remark()
+  // .use(remarkReact)
+  .use({settings: {breaks: true}})
+
+const singleLineProcessor = remark()
+  // .use(remarkReact)
+  .use({settings: {breaks: true}})
+  .use(disable, {block: ['list', 'atxHeading', 'setextHeading']})
+
+function disable(options) {
+  var proto = this.Parser.prototype;
+  ignore('blockMethods', options.block);
+  ignore('inlineMethods', options.inline);
+
+  function ignore(key, list) {
+    var values = proto[key];
+    var index = -1;
+    var length = list && list.length;
+    var pos;
+    while (++index < length) {
+      pos = values.indexOf(list[index]);
+      if (pos !== -1) {
+        values.splice(pos, 1);
+      }
+    }
+  }
+}
+
+const withAttribute = (name, value) => delta => delta.insert === '\n' ? delta : ({...delta, attributes: {...delta.attributes, [name]: value}});
+const withBlockAttribute = (name, value) => delta => delta.insert !== '\n' ? delta : ({...delta, attributes: {...delta.attributes, [name]: value}});
+
+const converters = {
+  html: node => [{insert: node.value}],
+  text: node => [{insert: node.value}],
+  break: node => [{insert: '\n'}],
+  root: node => convertNodes(node.children),
+  listItem: node => convertNodes(node.children),
+  paragraph: node => convertNodes(node.children).concat([{insert: '\n'}]),
+
+  // Inline formatting
+  link: node => convertNodes(node.children).map(withAttribute('link', node.url)),
+  // TODO maybe fix this or something
+  linkReference: node => convertNodes(node.children),
+  strong: node => convertNodes(node.children).map(withAttribute('bold', true)),
+  emphasis: node => convertNodes(node.children).map(withAttribute('emphasis', true)),
+  inlineCode: node => [{insert: node.value, attributes: {code: true}}],
+
+  // Block formatting
+  blockquote: node => convertNodes(node.children).map(withBlockAttribute('blockquote', true)),
+  list: node => convertNodes(node.children).map(withBlockAttribute('list', node.ordered ? 'ordered' : 'bullet')),
+  code: node => flatten(node.value.split('\n').map(line => [{insert: line}, {insert: '\n', attributes: {'code-block': true}}])),
+}
+
+const flatten = lists => [].concat(...lists)
+
+const convertNodes = nodes => flatten(nodes.map(convertMdAst))
+
+const convertMdAst = node => {
+  if (!converters[node.type]) {
+    console.log(JSON.stringify(node, null, 2))
+    fail
+  }
+  return converters[node.type](node)
+};
+
+const convertMarkdown = text => {
+  const ast = (text.indexOf('\n') === -1 ? singleLineProcessor : processor).parse(text);
+  return convertMdAst(ast)
+};
 
 const convertContents = node => {
   switch (node.type) {
@@ -45,13 +115,19 @@ const convertContents = node => {
     case undefined:
     case 'base':
     case 'normal':
+    case 'ordered_list':
+    case 'list':
     case 'todo':
-      // TODO markdown render
-      return [{insert: node.content + "\n"}]
+      return convertMarkdown(node.content)
     case 'symlink':
       return [{insert: {symlink: node.content}}, {insert: '\n'}]
+    case 'table':
+      return node.table.matrix.map(row => ({insert: row.join('|') + '\n'}))
     default:
-      return [{insert: JSON.stringify({...node, children: undefined}) + '\n'}]
+      const contents = JSON.stringify({...node, children: undefined})
+      console.log(contents)
+      fail
+      return [{insert: contents + '\n'}]
   }
 };
 
